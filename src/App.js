a37fbc6df8a20c896cf502e07dd080227f84b720
Kaskhalil87 <140751168+Kaskhalil87@users.noreply.github.com>
2024-02-23 16:03:14 -0600

    new-star-rating-app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import StarRating from "./components/star-rating";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <StarRating numOfStars={10} />
+    </div>
+  );
+}
+
+export default App;
